83eeb4e10831396ae69653a8c2ec69bca27028e5
itzvlad21 <150410926+itzvlad21@users.noreply.github.com>
2025-02-19 22:14:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const dataDir = path.join(__dirname, 'data');
+if (!fs.existsSync(dataDir)){
+    fs.mkdirSync(dataDir, { recursive: true });
+}
+
 require('dotenv').config();
 const express = require('express');
 const nodemailer = require('nodemailer');
